4d467ae4df7b3b02e8249329c4aea1d02e824553
Zarish Iqbal <zarishiqbal.zi@gmail.com>
2022-01-15 12:49:11 +0500

    add site under construction logo and text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-
+import warning from '../assets/warning.png'
+import Image from 'next/image'
 export const IndexPage = () => {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -10,6 +11,10 @@ export const IndexPage = () => {
 
 			<main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
 				<h1 className="text-6xl font-bold">Zarish Iqbal</h1>
+				<p className="my-5 flex items-center justify-center space-x-3">
+					<Image alt="under-construction" height={30} width={30} src={warning} />
+					<span> Site Under Construction</span>
+				</p>
 			</main>
 		</div>
 	)
